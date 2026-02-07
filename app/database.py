00fb2848f93cb22de20a8aff8e7d7750faa54c0b
devPallavi-superchat <pallavi.s@superchat.in>
2026-02-07 20:50:11 +0530

    hrms

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./hrms.db"  # easy to deploy & demo
 # For production you can switch to PostgreSQL via an env var
