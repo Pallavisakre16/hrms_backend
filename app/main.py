6b4a73f234c83e5974ab4aa19aca0ecd68eb62e9
devPallavi-superchat <pallavi.s@superchat.in>
2026-02-07 21:51:02 +0530

    hrms

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ origins = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://pallavis-hrms-lite-frontend.netlify.app"
 ]
 
 app.add_middleware(
